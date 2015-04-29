fde24c1989ecb22c92bf29fbc91bef6b20721d0d
bblens <23695158@qq.com>
2015-04-29 19:16:19 +0800

    完成遍历省市县三级列表的功能

--- src/com/coolweather/app/db/CoolWeatherDB.java
+++ src/com/coolweather/app/db/CoolWeatherDB.java
@@ -35,25 +35,25 @@ public class CoolWeatherDB {
 	public void saveProvince(Province province) {
 		if (province != null) {
 			ContentValues values = new ContentValues();
-			values.put(DBHelper.PROVINCE_NAME, province.getProvinceName());
-			values.put(DBHelper.PROVINCE_CODE, province.getProvinceCode());
-			db.insert(DBHelper.PROVINCE_TABLE_NAME, null, values);
+			values.put(DBHelper.COLUMN_PROVINCE_NAME, province.getProvinceName());
+			values.put(DBHelper.COLUMN_PROVINCE_CODE, province.getProvinceCode());
+			db.insert(DBHelper.TABLE_PROVINCE, null, values);
 		}
 	}
 
 	// 从数据库中读取全国省份信息
 	public List<Province> loadProvinces() {
 		List<Province> list = new ArrayList<Province>();
-		Cursor cursor = db.query(DBHelper.PROVINCE_TABLE_NAME, null, null,
+		Cursor cursor = db.query(DBHelper.TABLE_PROVINCE, null, null,
 				null, null, null, null);
 		if (cursor.moveToFirst()) {
 			do {
 				Province province = new Province();
 				province.setId(cursor.getInt(cursor.getColumnIndex("id")));
 				province.setProvinceName(cursor.getString(cursor
-						.getColumnIndex(DBHelper.PROVINCE_NAME)));
+						.getColumnIndex(DBHelper.COLUMN_PROVINCE_NAME)));
 				province.setProvinceCode(cursor.getString(cursor
-						.getColumnIndex(DBHelper.PROVINCE_CODE)));
+						.getColumnIndex(DBHelper.COLUMN_PROVINCE_CODE)));
 				list.add(province);
 			} while (cursor.moveToNext());
 
@@ -65,27 +65,27 @@ public class CoolWeatherDB {
 	public void saveCity(City city) {
 		if (city != null) {
 			ContentValues values = new ContentValues();
-			values.put(DBHelper.CITY_NAME, city.getCityName());
-			values.put(DBHelper.CITY_CODE, city.getCitycode());
-			values.put(DBHelper.CITY_PROVINCE_ID, city.getProvinceId());
-			db.insert(DBHelper.CITY_TABLE_NAME, null, values);
+			values.put(DBHelper.COLUMN_CITY_NAME, city.getCityName());
+			values.put(DBHelper.COLUMN_CITY_CODE, city.getCitycode());
+			values.put(DBHelper.COLUMN_CITY_PROVINCE_ID, city.getProvinceId());
+			db.insert(DBHelper.TABLE_CITY, null, values);
 		}
 	}
 
 	// 从数据库中读取某省城市信息
 	public List<City> loadCities(int provinceId) {
 		List<City> list = new ArrayList<City>();
-		Cursor cursor = db.query(DBHelper.CITY_TABLE_NAME, null,
-				DBHelper.CITY_PROVINCE_ID + " = ?",
+		Cursor cursor = db.query(DBHelper.TABLE_CITY, null,
+				DBHelper.COLUMN_CITY_PROVINCE_ID + " = ?",
 				new String[] { String.valueOf(provinceId) }, null, null, null);
 		if (cursor.moveToFirst()) {
 			do {
 				City city = new City();
 				city.setId(cursor.getInt(cursor.getColumnIndex("id")));
 				city.setCityName(cursor.getString(cursor
-						.getColumnIndex(DBHelper.CITY_NAME)));
+						.getColumnIndex(DBHelper.COLUMN_CITY_NAME)));
 				city.setCitycode(cursor.getString(cursor
-						.getColumnIndex(DBHelper.CITY_CODE)));
+						.getColumnIndex(DBHelper.COLUMN_CITY_CODE)));
 				city.setProvinceId(provinceId);
 				list.add(city);
 			} while (cursor.moveToNext());
@@ -98,27 +98,27 @@ public class CoolWeatherDB {
 	public void saveCounty(County county) {
 		if (county != null) {
 			ContentValues values = new ContentValues();
-			values.put(DBHelper.COUNTY_NAME, county.getCountyName());
-			values.put(DBHelper.COUNTY_CODE, county.getCountyCode());
-			values.put(DBHelper.COUNTY_CITY_ID, county.getCityId());
-			db.insert(DBHelper.COUNTY_TABLE_NAME, null, values);
+			values.put(DBHelper.COLUMN_COUNTY_NAME, county.getCountyName());
+			values.put(DBHelper.COLUMN_COUNTY_CODE, county.getCountyCode());
+			values.put(DBHelper.COLUMN_COUNTY_CITY_ID, county.getCityId());
+			db.insert(DBHelper.TABLE_COUNTY, null, values);
 		}
 	}
 
 	// 从数据库中读取某省城市信息
 	public List<County> loadCounties(int cityId) {
 		List<County> list = new ArrayList<County>();
-		Cursor cursor = db.query(DBHelper.COUNTY_TABLE_NAME, null,
-				DBHelper.COUNTY_CITY_ID + " = ?",
+		Cursor cursor = db.query(DBHelper.TABLE_COUNTY, null,
+				DBHelper.COLUMN_COUNTY_CITY_ID + " = ?",
 				new String[] { String.valueOf(cityId) }, null, null, null);
 		if (cursor.moveToFirst()) {
 			do {
 				County county = new County();
 				county.setId(cursor.getInt(cursor.getColumnIndex("id")));
 				county.setCountyName(cursor.getString(cursor
-						.getColumnIndex(DBHelper.COUNTY_NAME)));
+						.getColumnIndex(DBHelper.COLUMN_COUNTY_NAME)));
 				county.setCountyCode(cursor.getString(cursor
-						.getColumnIndex(DBHelper.COUNTY_CODE)));
+						.getColumnIndex(DBHelper.COLUMN_COUNTY_CODE)));
 				county.setCityId(cityId);
 				list.add(county);
 			} while (cursor.moveToNext());
--- src/com/coolweather/app/db/DBHelper.java
+++ src/com/coolweather/app/db/DBHelper.java
@@ -9,38 +9,38 @@ public final class DBHelper {
 	public static final int VERSION = 1;
 
 	// 省表字段
-	public static final String PROVINCE_TABLE_NAME = "Province";
-	public static final String PROVINCE_NAME = "province_name";
-	public static final String PROVINCE_CODE = "province_code";
+	public static final String TABLE_PROVINCE = "Province";
+	public static final String COLUMN_PROVINCE_NAME = "province_name";
+	public static final String COLUMN_PROVINCE_CODE = "province_code";
 
 	// 市表字段
-	public static final String CITY_TABLE_NAME = "City";
-	public static final String CITY_NAME = "city_name";
-	public static final String CITY_CODE = "city_code";
-	public static final String CITY_PROVINCE_ID = "province_id";
+	public static final String TABLE_CITY = "City";
+	public static final String COLUMN_CITY_NAME = "city_name";
+	public static final String COLUMN_CITY_CODE = "city_code";
+	public static final String COLUMN_CITY_PROVINCE_ID = "province_id";
 
 	// 县表字段
-	public static final String COUNTY_TABLE_NAME = "County";
-	public static final String COUNTY_NAME = "county_name";
-	public static final String COUNTY_CODE = "county_code";
-	public static final String COUNTY_CITY_ID = "city_id";
+	public static final String TABLE_COUNTY = "County";
+	public static final String COLUMN_COUNTY_NAME = "county_name";
+	public static final String COLUMN_COUNTY_CODE = "county_code";
+	public static final String COLUMN_COUNTY_CITY_ID = "city_id";
 
 	// 省份建表 SQL
 	public static final String CREATE_TABLE_PROVINCT = "create table "
-			+ PROVINCE_TABLE_NAME + " ( "
-			+ "id integer primary key autoincrement, " + PROVINCE_NAME
-			+ " text, " + PROVINCE_CODE + " text)";
+			+ TABLE_PROVINCE + " ( "
+			+ "id integer primary key autoincrement, " + COLUMN_PROVINCE_NAME
+			+ " text, " + COLUMN_PROVINCE_CODE + " text)";
 
 	// 城市建表 SQL
 	public static final String CREATE_TABLE_CITY = "create table "
-			+ CITY_TABLE_NAME + " ( "
-			+ "id integer primary key autoincrement, " + CITY_NAME + " text, "
-			+ CITY_CODE + " text, " + CITY_PROVINCE_ID + " integer)";
+			+ TABLE_CITY + " ( "
+			+ "id integer primary key autoincrement, " + COLUMN_CITY_NAME + " text, "
+			+ COLUMN_CITY_CODE + " text, " + COLUMN_CITY_PROVINCE_ID + " integer)";
 
 	// 县建表 SQL
 	public static final String CREATE_TABLE_COUNTY = "create table "
-			+ COUNTY_TABLE_NAME + " ("
-			+ "id integer primary key autoincrement, " + COUNTY_NAME
-			+ " text, " + COUNTY_CODE + " text, " + COUNTY_CITY_ID
+			+ TABLE_COUNTY + " ("
+			+ "id integer primary key autoincrement, " + COLUMN_COUNTY_NAME
+			+ " text, " + COLUMN_COUNTY_CODE + " text, " + COLUMN_COUNTY_CITY_ID
 			+ " integer)";
 }
